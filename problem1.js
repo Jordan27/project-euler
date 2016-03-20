a973e075950be97c169df319c34317b6757fa5e4
Jordan Adams <jsadams27@gmail.com>
2016-03-20 15:15:41 -0500

    added additional details to problem1.js file

--- problem1.js
+++ problem1.js
@@ -1,6 +1,14 @@
 //source of this exercise can be 
 //found here: http://projecteuler.net/problem=1 
 
+//If we list all the natural numbers below 10 
+//that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+//The sum of these multiples is 23.
+
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+
 var total = 0;
 
 for (x = 0; x < 1000; x++) {
